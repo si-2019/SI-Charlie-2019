bd8780ff75fefc43a7f2244b2e60795b5dec8472
Edo Skaljo <eskaljo1@etf.unsa.ba>
2019-04-13 13:32:14 +0200

    Napravljen zahtjev za predmete

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express=require('express');
 var app = express();
 //
 
+//get zahtjev za predmete
+app.get('/api/predmeti', (req, res) => {
+    
+});
+//
+
 //Server
 const port = 5000;
 app.listen(port, () => console.log('Server pokrenut'));
