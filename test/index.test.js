5d759680588017eb4ae30b4a2eead61328d02fc1
Edo Skaljo <eskaljo1@etf.unsa.ba>
2019-04-13 17:09:25 +0200

    početak testa

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,11 @@
+//chai
 var chai = require('chai');
 chaiHttp = require('chai-http');
-
 chai.use(chaiHttp);
+//
+
+const  app = require('./index.js');
+
+describe('pokreni server', () => {
+  
+});
