4f1c4fa9ac5ac8e8bd21a8956465fe8fe7cab53c
amirmu-at-587105673553 <amuminovic3@etf.unsa.ba>
2019-04-27 17:42:29 +0200

    Poboljsana struktura

---
+++ server/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = process.env.PORT || 3001;
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+//
+
+//get zahtjev za predmete
+app.get("/api/predmeti", (req, res) => {
+  const predmeti = [
+    { naziv: "Softverski inženjering", br_studenata: 150 },
+    { naziv: "Logički dizajn", br_studenata: 165 },
+    { naziv: "Računarske arhitekture", br_studenata: 170 }
+  ];
+  res.status(200);
+  res.json(predmeti);
+});
+
+// let ispiti = [{ id: 5, ispit: "LD" }];
+
+// app.get("/ispiti", (req, res) => {
+//   res.send(JSON.stringify(ispiti));
+// });
+
+// //Brisanje ispita
+// app.delete("/ispit/:ispitID", async (req, res) => {
+//   const { ispitID } = req.params;
+//   console.log(ispitID);
+//   //TODO: Provjeri da li je korisnik profesor
+//   //Provjeri da li je validan id
+//   //Provjeri ako ima taj ispit u bazi
+//   // Obrisi;
+//   try {
+//     ispiti = await ispiti.filter(ispit => ispit.id != ispitID);
+//     console.log(ispiti);
+//     res.send("Uspjesno obrisan ispit");
+//   } catch (err) {
+//     res.send(err.message);
+//   }
+// });
+
+//Server
+app.listen(port, () => console.log(`Server pokrenut na portu ${port}`));
+//
+
+module.exports = app;
---
+++ server/server.test.js
@@ -0,0 +1,20 @@
+//chai
+var chai = require("chai");
+chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+//
+
+const app = require("../test/index.js");
+
+describe("pokreni server", () => {
+  it("dobavlja predmete", done => {
+    chai
+      .request(app)
+      .get("/api/predmeti")
+      .end((err, res) => {
+        chai.expect(res).to.have.status(200);
+        chai.expect(res.body).to.have.lengthOf(3);
+        done();
+      });
+  });
+});
